77c19cd0f90b548b1a5a47d74918be29636fa5f7
deepashree2 <deepashreesl96@gmail.com>
2023-06-27 22:09:24 +0530

    heloo git

---
+++ Eng.java
@@ -0,0 +1,26 @@
+class Eng{
+	public static void main(String[] args){
+	System.out.println(" Java\n");
+	System.out.println(" Python\n");
+	System.out.println(" c\n");
+	System.out.println(" c++\n");
+	System.out.println(" R\n");
+	System.out.println(" Datascience\n");
+	System.out.println(" IOT\n");
+	System.out.println(" JAVA FULL STACK\n");
+	System.out.println(" PYTHON FULL STACK\n");
+	System.out.println(" JAVA DEVELOPER\n");
+	System.out.println(" PYTHON DEVELOPER\n");
+	System.out.println(" SOFTWARE ENGINNER\n");
+	System.out.println(" SOFTWARE DEVELOPPER\n");
+	System.out.println(" HTML\n");
+	System.out.println(" PHP\n");
+	System.out.println(" JS\n");
+	System.out.println(" XAMMP\n");
+	System.out.println(" ORACLE\n");
+	System.out.println(" SQL\n");
+	System.out.println(" MYSQL\n");
+	System.out.println(" INTERPREATOR\n");
+	System.out.println("JAVA \n");
+}
+}
